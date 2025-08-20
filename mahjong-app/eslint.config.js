1e8b2750fc7feb406f2c61540cb5be81080145ef
RiLLiaN <srin5ame@gmail.com>
2025-08-21 05:04:07 +0900

    fix: update ESLint config for PR check2

--- mahjong-app/eslint.config.js
+++ mahjong-app/eslint.config.js
@@ -1,4 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser"; // ←これが重要
 import path from "path";
 import url from "url";
 
@@ -13,4 +14,21 @@ export default [
     ...compat.extends("eslint:recommended"),
     ...compat.extends("plugin:@typescript-eslint/recommended"),
     ...compat.extends("next/core-web-vitals"),
+
+    {
+        languageOptions: {
+            parser: tsParser, // ←文字列じゃなくて import したオブジェクト
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: "module",
+                project: "./tsconfig.json",
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { argsIgnorePattern: "^_" },
+            ],
+        },
+    },
 ];
